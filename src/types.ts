9ad0781a3a05a8f357af1cfa418e17b07fb8f3c4
paulocarvalho-ia <pcclima.ia@gmail.com>
2026-08-12 21:18:35 -0300

    Add Musica interface to types.ts

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface Musica {
+  id: string;
+  titulo: string;
+  autor: string;
+  conteudo: string; // texto completo com cifras e letras
+  criadaEm: number;
+  alteradaEm: number;
+}
